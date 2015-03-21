af4f74d89d07726cd2d02e91e2dea6f91f091bd9
sanvazfra <sanvazfra@users.noreply.github.com>
2015-03-21 00:35:38 -0600

    Agregue el métido despedi
    Agregue un nuevo método para despedirse...

--- Exercise2/src/com/app/controller/Aplication.java
+++ Exercise2/src/com/app/controller/Aplication.java
@@ -6,6 +6,10 @@ public class Aplication {
 		System.out.println("Hola mundo!!!");
 		System.out.println("Hola mundo!!!");
 		System.out.println("Hola mundo!!!");
+		despedir();
+	}
+	public void despedir(){
+		System.out.println("Hola mundo!!!");
 	}
 
 }
